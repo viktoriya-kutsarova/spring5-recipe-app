b5a22aa2e710145f1050e1e2da5e87454e460f4b
Viktoriya Kutsarova <viktoriya.kutsarova@gmail.com>
2020-04-03 11:27:19 +0200

    Set UnitOfMeasuer fetch type explicitly.

--- src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -3,6 +3,7 @@ package guru.springframework.spring5recipeapp.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,7 +21,7 @@ public class Ingredient {
 	private Long id;
 	private String description;
 	private BigDecimal amount;
-	@OneToOne
+	@OneToOne(fetch = FetchType.EAGER)
 	private UnitOfMeasure unit;
 
 	@ManyToOne
